cdb01df8d8245e713c36bc2cb25ab51a86268a12
HarshP34 <hupatel1005@gmail.com>
2022-11-21 14:51:24 +0530

    task-23 Expensetracker

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -2,6 +2,9 @@ import {Router} from 'express';
 
 import {Todo} from '../models/todo';
 
+type RequestBody={text:string};
+type RequestParams={todoId:string}
+
 let todos:Todo[]=[];
 
 const router =Router();
@@ -11,16 +14,18 @@ router.get('/',(req,res,next)=>{
 })
 
 router.post('/todo',(req,res,next)=>{
+    const body =req.body as RequestBody
     const newTodo:Todo={
         id:new Date().toISOString(),
-        text:req.body.text
+        text:body.text
     };
     todos.push(newTodo);
     res.status(201).json({message:'Added todo',todo:newTodo,todos:todos})
 })
 
 router.put('/todo/:todoId',(req,res,next)=>{
-    const tid=req.params.todoId;
+    const params=req.params as RequestParams
+    const tid=params.todoId;
     const todoIndex=todos.findIndex(todoItem=>todoItem.id===tid);
     if(todoIndex>=0)
     {
@@ -32,7 +37,8 @@ return res.status(200).json({message:'Updated todo',todos:todos})
 
 
 router.delete('/todo/:todoId',(req,res,next)=>{
-    todos=todos.filter(todoItem=>todoItem.id!=req.params.todoId);
+    const params=req.params as RequestParams
+    todos=todos.filter(todoItem=>todoItem.id!=params.todoId);
     res.status(200).json({message:'Deleted todo',todos:todos});
 })
 
